44e86616de1fd6ae852875431414dedaef32a7ff
RohamAbootorabian <abootorabianamir@gmail.com>
2025-05-05 02:04:27 +0330

    Initial first commit for AP_Project
    add some method for File class and Directory class

--- main.py
+++ main.py
@@ -3,11 +3,60 @@ class File:
         self.name = name
         self.content = content
 
+    def touch(self):
+        pass
+
+    def rm(self):
+        pass
+
+    def new_file_txt(self):
+        pass
+
+    def append(self):
+        pass
+
+    def edit_line(self):
+        pass
+
+    def deline(self):
+        pass
+
+    def cat(self):
+        pass
+
+    def mv(self):
+        pass
+
+    def cp(self):
+        pass
+
+    def rename(self):
+        pass
+
 
 class Directory:
     def __init__(self, name):
         self.name = name
-        self.contents = {}  
+        self.contents = {}
+
+    def mkdir(self):
+        pass
+
+    def rmdir(self):
+        pass
+
+    def mv(self):
+        pass
+
+    def cp(self):
+        pass
+
+    def rename(self):
+        pass
+
+    def ls(self):
+        pass
+
 
 class VirtualFileSystem:
     def __init__(self):
@@ -15,11 +64,18 @@ class VirtualFileSystem:
         self.current = self.root
         self.path = []
 
+    def cd(self):
+        pass
+
 
 class CommandPrommt:
-    def __init__(self,user):
+    def __init__(self, user):
         self.commands = {}
         self.user = user
 
     def read_line(self):
         pass
+
+
+if __name__ == "__main__":
+    pass
